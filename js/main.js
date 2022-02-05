621c132c6409f5201b0b758a235f404580ec885c
Harvi91 <thebeauty.by@gmail.com>
2022-02-05 12:35:14 +0300

    add file

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(function () {
+  let text = $(".header-contacts").html(
+    '<div class="header-contacts"><span class="phone-code"> +375 <span class=prefix>(29)</span></span> 111-13-33 <br /><p>звоните с 10 до 21</p></div>'
+  );
+});
